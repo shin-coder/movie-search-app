82a94d6c8098f6e3d2a6472f289875064e9e9390
shin-coder <kawaguchi.shin.03@gmail.com>
2025-01-13 14:36:35 +0900

    view moreボタンの追加とデータ更新紐付け作業

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,27 @@ import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [page, setPage] = useState(1);
   const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
   const API_URL = import.meta.env.VITE_TMDB_API_URL;
   const IMG_PATH = import.meta.env.VITE_TMDB_IMG_PATH;
 
   useEffect(() => {
     fetch(
-      `${API_URL}?api_key=${API_KEY}&include_adult=false&include_video=false&language=jp-JP&primary_release_year=2024&sort_by=primary_release_date.desc`
+      `${API_URL}?api_key=${API_KEY}&include_adult=false&include_video=false&language=ja&primary_release_year=2024&sort_by=primary_release_date.desc&page=${page}`
     )
       .then((response) => response.json())
       .then((data) => {
-        setMovies(data.results);
+        setMovies((prev) =>
+          page === 1 ? data.results : [...prev, ...data.results]
+        );
       });
-  }, [API_KEY, API_URL]);
+  }, [API_KEY, API_URL, page]);
+
+  const loadMore = () => {
+    setPage((prev) => prev + 1);
+  };
+
   return (
     <>
       <div className="container">
@@ -33,6 +41,9 @@ function App() {
             <h2 className="movieTitle">{movie.title}</h2>
           </div>
         ))}
+      </div>
+      <div className="load-more">
+        <button onClick={loadMore}>さらに読み込む</button>
       </div>
     </>
   );
